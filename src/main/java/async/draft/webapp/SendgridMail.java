211bfb3892d80f5a3c425116c4c402a0cc061679
Eric Boivin <ericboivin@gmail.com>
2015-06-06 01:34:06 -0400

    Template with draft code

--- src/main/java/async/draft/webapp/SendgridMail.java
+++ src/main/java/async/draft/webapp/SendgridMail.java
@@ -13,7 +13,7 @@ public class SendgridMail {
 		email.addTo(player.getEmail());
 		email.setFrom("ericboivin@gmail.com");
 		email.setSubject("Netrunner Identity Draft");
-		email.setHtml("<html><body>Hi "+player.getName()+"! It's your turn to select your pick for the Netrunner draft. Click the following link to get to the page and do your pick.<br /><br /><a href=\"https://netrunner-faction-draft.herokuapp.com/identitydraft?token="+token+"\">Do your pick</a><br /><br /><a href=\"https://netrunner-faction-draft.herokuapp.com/showdraft?draft="+draft+"\">Click here to see all picks and the draft order</a></body></html>");
+		email.setHtml("<html><body>Hi "+player.getName()+"! It's your turn to select your pick for the Netrunner draft. Click the following link to get to the page and do your pick.<br /><br /><a href=\"https://netrunner-faction-draft.herokuapp.com/identitydraft?draft=&"+draft+"token="+token+"\">Do your pick</a><br /><br /><a href=\"https://netrunner-faction-draft.herokuapp.com/showdraft?draft="+draft+"\">Click here to see all picks and the draft order</a></body></html>");
 
 		try {
 			SendGrid.Response response = sendgrid.send(email);
