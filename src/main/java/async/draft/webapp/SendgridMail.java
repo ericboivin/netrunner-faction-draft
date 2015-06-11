3a51d095dd3c6ae43f1b62236a9d7fa10415d2d1
Eric Boivin <eric.boivin1@bell.ca>
2015-06-11 11:01:35 -0400

    Emails without <a> hyperlinks

--- src/main/java/async/draft/webapp/SendgridMail.java
+++ src/main/java/async/draft/webapp/SendgridMail.java
@@ -15,23 +15,19 @@ public class SendgridMail {
 		email.setSubject("Netrunner Identity Draft");
 		StringBuffer html = new StringBuffer("<html><body>Hi ");
 		html.append(player.getName());
-		html.append("! It's your turn to select your pick for the Netrunner draft. Click the following link to get to the page and do your pick.<br /><br /><a href=\"https://netrunner-faction-draft.herokuapp.com/identitydraft?draft=");
+		html.append("! It's your turn to select your pick for the Netrunner draft. Click the following link to get to the page and do your pick.<br />https://netrunner-faction-draft.herokuapp.com/identitydraft?draft=");
 		html.append(draft);
 		html.append("&token=");
 		html.append(token);
-		html.append("\">Do your pick</a><br /><br /><a href=\"https://netrunner-faction-draft.herokuapp.com/showdraft?draft=");
+		html.append("<br /><br />");
+		html.append("\">Click here to see all picks and the draft order: <br />https://netrunner-faction-draft.herokuapp.com/showdraft?draft=");
 		html.append(draft);
-		html.append("\">Click here to see all picks and the draft order</a><br /><br /><small>Problems clicking the link? Use this instead: https://netrunner-faction-draft.herokuapp.com/identitydraft?draft=");
-		html.append(draft);
-		html.append("&token=");
-		html.append(token);
-		html.append("</small>");
 		html.append("</body></html>");
 		
 		email.setHtml(html.toString());
 
 		try {
-			SendGrid.Response response = sendgrid.send(email);
+			sendgrid.send(email);
 		} catch (SendGridException e) {
 			System.out.println(e);
 		}
